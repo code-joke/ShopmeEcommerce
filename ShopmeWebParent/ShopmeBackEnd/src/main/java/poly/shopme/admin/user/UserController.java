24ef1096422bc4a493f1dbe3f15a601e9ada8c2d
vinh <vinhtqph09311@gmail.com>
2021-02-23 11:39:49 +0700

    vinhtq update code

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/poly/shopme/admin/user/UserController.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/poly/shopme/admin/user/UserController.java
@@ -1,15 +1,20 @@
 package poly.shopme.admin.user;
 
+import java.io.IOException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import poly.shopme.admin.FileUploadUtil;
 import poly.shopme.common.entity.Role;
 import poly.shopme.common.entity.User;
 
@@ -40,11 +45,25 @@ public class UserController {
 	}
 	
 	@PostMapping("/users/save")
-	public String saveUser(User user, RedirectAttributes redirectAttributes) {
-		service.save(user);
+	public String saveUser(User user, RedirectAttributes redirectAttributes,
+			@RequestParam("image") MultipartFile multipartFile) throws IOException {
+		
+		if(!multipartFile.isEmpty()) {
+			String fileName = StringUtils.cleanPath(multipartFile.getOriginalFilename());
+			user.setPhotos(fileName);
+			User savedUser = service.save(user);
+			
+			String uploadDir = "user-photos/" + savedUser.getId();
+			
+			FileUploadUtil.cleanDir(uploadDir);
+			FileUploadUtil.saveFile(uploadDir, fileName, multipartFile);
+		} else {
+			if(user.getPhotos().isEmpty()) user.setPhotos(null);
+			service.save(user);
+		}
 		
 		redirectAttributes.addFlashAttribute("message", "Lưu thành công");
-		
+
 		return "redirect:/users";
 	}
 	
@@ -88,7 +107,7 @@ public class UserController {
 			RedirectAttributes redirectAttributes) {
 		service.updateUserEnabledStatus(id, enabled);
 		String status = enabled ? "kích hoạt" : "hủy kích hoạt";
-		String message = "Tài khoản (ID: " + id + ") đã được " + status + " thành công";
+		String message = "Tài khoản đã được " + status + " thành công " + "(ID: " + id + ")";
 		redirectAttributes.addFlashAttribute("message", message);
 		
 		return "redirect:/users";
